61f486ffbc96518758e09dcd519444621b8d6889
stevebrush <steevebrush@gmail.com>
2018-04-10 19:19:19 +0200

    cleanup the mess and start working on block list

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// settings
+var api = "https://explorer.beta.ird.cash";
+// var api = "http://localhost:13007";
+var api_blockexplorer ="http://localhost:13007";
+var coinDifficultyTarget=175;
+var symbol="IRD";
+var coinUnits=100000000;
+var blockchainExplorer = "https://explorer.ird.cash/?hash={id}#blockchain_block";
