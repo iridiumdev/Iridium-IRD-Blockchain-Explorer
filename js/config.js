ebb28620603dee07157bcec68be77b494066da88
stevebrush <steevebrush@gmail.com>
2018-04-28 01:39:16 +0200

    Transactions are ok now

--- js/config.js
+++ js/config.js
@@ -1,7 +1,5 @@
 // settings
 var api = "https://explorer.beta.ird.cash";
-// var api = "http://localhost:13007";
-var api_blockexplorer ="http://localhost:13007";
 var coinDifficultyTarget=175;
 var symbol="IRD";
 var coinUnits=100000000;
