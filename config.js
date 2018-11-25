ab2872aaa2f33f36ea7f489e248bcc35e8d99092
stevebrush <steevebrush@gmail.com>
2018-11-25 22:57:31 +0100

    Remove iridium.cnpools.space and https://ird.semipool.com Show MM pools

--- config.js
+++ config.js
@@ -27,18 +27,18 @@ const poolList = [
     {"id":17,"visible":true,"url":"https://matamu.me/iridium","api":"https://matamu.me:5119","status":false,"flag":"DE","loc":"Germany"},
     {"id":18,"visible":true,"url":"https://iridium.clevery.xyz","api":"https://iridium.clevery.xyz/api","status":true,"flag":"FR","loc":"France"},
     {"id":19,"visible":true,"url":"https://ird.soyminero.es","api":"https://mig.ird.cash/soyminero","status":true,"flag":"ES","loc":"Spain"},
-    {"id":20,"visible":true,"url":"https://iridium.cnpools.space","api":"https://iridium.cnpools.space:8119","status":true,"flag":"US","loc":"New-York"},
+    {"id":20,"visible":true,"url":"https://iridium.cnpools.space","api":"https://iridium.cnpools.space:8119","status":false,"flag":"US","loc":"New-York"},
     // {"id":21,"visible":true,"url":"http://irdpool.cryptonine.my","api":"https://mig.ird.cash/cryptonine","status":true,"flag":"SG","loc":"Singapore"},
     {"id":22,"visible":true,"url":"http://iridium.miningpoolhouse.com","api":"https://mig.ird.cash/mph","status":false,"flag":"DE","loc":"Germany"},
     {"id":23,"visible":true,"url":"https://ird.codpool.com","api":"https://pool1.codpool.com:8118","status":true,"flag":"US","loc":"New-York"},
     {"id":24,"visible":true,"url":"http://ird.wahaobi.com","api":"https://mig.ird.cash/hk","status":false,"flag":"CN","loc":"Hong Kong"},
     {"id":25,"visible":true,"url":"https://iridium.smartcoinpool.com","api":"https://iridium.smartcoinpool.com:9019","status":true,"flag":"US","loc":"Tennessee"},
-    {"id":26,"visible":true,"url":"https://ird.semipool.com","api":"https://mig.ird.cash/semipool","status":true,"flag":"FR","loc":"France"}
+    {"id":26,"visible":true,"url":"https://ird.semipool.com","api":"https://mig.ird.cash/semipool","status":false,"flag":"FR","loc":"France"}
 ];
 const mmList = [
-    {"id":1,"visible":true,"pair":"aeon",url:"https://aeon.semipool.com","api":"https://aeon.semipool.com/api/pool/altblocks/ird?page=0&limit=20","status":true,"flag":"FR","loc":"France"},
-    {"id":2,"visible":true,"pair":"aeon",url:"https://xtri.semipool.com","api":"https://xtri.semipool.com/api/pool/altblocks/ird?page=0&limit=20","status":true,"flag":"FR","loc":"France"},
-    {"id":3,"visible":true,"pair":"aeon",url:"https://webarq.semipool.com","api":"https://webarq.semipool.com/api/pool/altblocks/ird?page=0&limit=20","status":true,"flag":"FR","loc":"France"}
+    {"id":1,"visible":true,"pair":"AEON",url:"https://aeon.semipool.com","api":"https://aeon.semipool.com/api/pool/altblocks/ird?page=0&limit=20","status":true,"flag":"FR","loc":"France"},
+    {"id":2,"visible":true,"pair":"XTRI",url:"https://xtri.semipool.com","api":"https://xtri.semipool.com/api/pool/altblocks/ird?page=0&limit=20","status":true,"flag":"FR","loc":"France"},
+    {"id":3,"visible":true,"pair":"ARQ",url:"https://webarq.semipool.com","api":"https://webarq.semipool.com/api/pool/altblocks/ird?page=0&limit=20","status":true,"flag":"FR","loc":"France"}
 ];
 
 const seedsNodes = [
