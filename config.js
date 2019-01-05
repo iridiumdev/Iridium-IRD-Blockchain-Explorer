aa024fe49115169d4d2023b077cd84a91c99b1bd
stevebrush <steevebrush@gmail.com>
2019-01-05 15:49:07 +0100

    Add MM pool https://arqma.minercountry.com

--- config.js
+++ config.js
@@ -41,7 +41,8 @@ const mmList = [
     {"id":3,"visible":true,"pair":"ARQ",url:"https://webarq.semipool.com","api":"https://webarq.semipool.com/api/pool/altblocks/ird?page=0&limit=20","status":true,"flag":"FR","loc":"France"},
     {"id":4,"visible":true,"pair":"AEON",url:"https://aeon.minercountry.com/ird","api":"https://aeon.minercountry.com/irdapi/stats","status":true,"flag":"GB","loc":"London"},
     {"id":5,"visible":true,"pair":"ARQ",url:"https://arqidium.smartcoinpool.com","api":"https://iridium.smartcoinpool.com:9019/stats","status":true,"flag":"US","loc":"Tennessee"},
-    {"id":6,"visible":true,"pair":"AEON",url:"https://aeon.hackerknowledge.de","api":"https://pool.ird.hackerknowledge.de:8319/stats","status":true,"flag":"DE","loc":"Germany"}
+    {"id":6,"visible":true,"pair":"AEON",url:"https://aeon.hackerknowledge.de","api":"https://pool.ird.hackerknowledge.de:8319/stats","status":true,"flag":"DE","loc":"Germany"},
+    {"id":7,"visible":true,"pair":"ARQ",url:"https://arqma.minercountry.com","api":"https://arqma.minercountry.com/mapi/stats","status":true,"flag":"GB","loc":"London"}
 ];
 
 const seedsNodes = [
