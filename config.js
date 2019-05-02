94122dc9efde94ed36e1b43f0c274777260fb876
Stevebrush <steevebrush@gmail.com>
2019-05-02 22:32:47 +0200

    Fix MM handle when no response remove iridium-arqma.bluerockpools.net and iridiumpool.duckdns.org ad they died

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const poolList = [
     {"id":3,"visible":true,"url":"https://irdpool.ca","api":"https://irdpool.ca:8117","status":true,"flag":"CA","loc":"Canada"},
     {"id":4,"visible":true,"url":"https://irdpool.net","api":"https://irdpool.net:8117","status":true,"flag":"AU","loc":"Australia"},
     {"id":5,"visible":true,"url":"https://cryptoknight.cc/iridium","api":"https://cryptoknight.cc/rpc/iridium","status":true,"flag":"DE","loc":"Germany"},
-    {"id":6,"visible":true,"url":"http://iridiumpool.duckdns.org","api":"https://mig.ird.cash/nl","status":true,"flag":"NL","loc":"Netherlands"},
+    {"id":6,"visible":true,"url":"http://iridiumpool.duckdns.org","api":"https://mig.ird.cash/nl","status":false,"flag":"NL","loc":"Netherlands"},
     {"id":7,"visible":true,"url":"http://ird.cryptoisme.com","api":"https://mig.ird.cash/cryptoisme","status":true,"flag":"ID","loc":"Indonesia"},
     {"id":8,"visible":true,"url":"https://coinpoolit.webhop.me/ird","api":"https://ns3128504.ip-51-68-204.eu:8119","status":true,"flag":"IT","loc":"Italy"},
     {"id":9,"visible":true,"url":"https://ird.soyminero.es","api":"https://mig.ird.cash/soyminero","status":true,"flag":"ES","loc":"Spain"},
@@ -29,9 +29,8 @@ const mmList = [
     {"id":5,"visible":true,"pair":"ARQ",url:"https://arqidium.smartcoinpool.com","api":"https://iridium.smartcoinpool.com:9019/stats","status":true,"flag":"US","loc":"Tennessee"},
     {"id":6,"visible":true,"pair":"AEON",url:"https://aeon.hackerknowledge.de","api":"https://pool.ird.hackerknowledge.de:8319/stats","status":true,"flag":"DE","loc":"Germany"},
     {"id":7,"visible":true,"pair":"ARQ",url:"https://arqma.minercountry.com","api":"https://arqma.minercountry.com/mapi/stats","status":true,"flag":"GB","loc":"London"},
-    {"id":8,"visible":true,"pair":"ARQ",url:"https://arq.hackerknowledge.de","api":"https://pool.ird.hackerknowledge.de:8319/stats","status":true,"flag":"DE","loc":"Germany"},
     {"id":9,"visible":true,"pair":"XTRI",url:"https://xtridium.minercountry.com","api":"https://xtridium.minercountry.com/mapi/stats","status":true,"flag":"CA","loc":"Montreal"},
-    {"id":10,"visible":true,"pair":"ARQ",url:"https://iridium-arqma.bluerockpools.net","api":"https://iridium-arqma.bluerockpools.net:8114/stats","status":true,"flag":"US","loc":"Minneapolis"}
+    {"id":10,"visible":true,"pair":"ARQ",url:"https://iridium-arqma.bluerockpools.net","api":"https://iridium-arqma.bluerockpools.net:8114/stats","status":false,"flag":"US","loc":"Minneapolis"}
 ];
 
 const seedsNodes = [
